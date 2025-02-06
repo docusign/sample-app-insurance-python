d2c0d21181b9df05dba2e7c2cc414ce04221b96a
inbargazit <inbar.gazit@docusign.com>
2025-02-06 15:13:00 -0800

    Renaming Sample App from MySure to "Insurance Sample App"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ export const Footer = () => {
   return (
     <footer role="contentinfo" className="footer bg-white pt-4 pb-4">
       <div className="container">
-        <span className="copyright">{t("Copyright")}</span>
       </div>
     </footer>
   );
