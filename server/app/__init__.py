4629679dc1ca0d550492581bf68196ad3808f32f
karissarjacobsen <73908223+karissarjacobsen@users.noreply.github.com>
2025-10-15 14:46:25 -0700

    Fix karissa (#81)
    * switched to server-side sessions (Flask-Session + filesystem)

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
+from flask_session import Session
 
 from app.api import clickwrap, requests, common, auth
-
 
 load_dotenv()
 
@@ -11,8 +11,21 @@ URL_PREFIX = '/api'
 
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
+
+# Add server-side session config
+app.config.update(
+    SESSION_TYPE="filesystem",             
+    SESSION_FILE_DIR="/tmp/flask_session",
+    SESSION_PERMANENT=False,
+    SESSION_USE_SIGNER=True,
+    SESSION_COOKIE_NAME="sid"        
+)
+
+Session(app) 
+
 app.register_blueprint(clickwrap, url_prefix=URL_PREFIX)
 app.register_blueprint(common, url_prefix=URL_PREFIX)
 app.register_blueprint(requests, url_prefix=URL_PREFIX)
 app.register_blueprint(auth, url_prefix=URL_PREFIX)
+
 cors = CORS(app)
